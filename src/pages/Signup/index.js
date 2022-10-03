b7a02ecb6678c537d8885562c62ab436a222f91e
RHainz <rogerhainz@gmail.com>
2022-10-03 16:35:43 -0300

    pulling

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,90 +1,141 @@
 import { useState } from "react";
 import { api } from "../../api/api";
 import { useNavigate } from "react-router-dom";
+import {Formik, Field, Form} from "formik";
 
 export function Signup() {
-  const navigate = useNavigate();
-  const [form, setForm] = useState({
-    name: "",
+  // const navigate = useNavigate();
+  // const [form, setForm] = useState({
+  //   name: "",
+  //   email: "",
+  //   password: "",
+  //   confirmPassword: "",
+  // });
+
+function onSubmit (values, actions){
+    console.log("Submit", values)
+  }
+
+return (
+  <div>
+<Formik 
+  onSubmit={onSubmit}
+  initialValues={{
+    name: "", 
     email: "",
-    password: "",
-    confirmPassword: "",
-  });
+  }}         
+  render={({values})=> (
+  <Form>
+  <div>
+    <label>Nome</label> {values.name}
+    <Field name="name" type="text" />
+    
+    <label>E-mail</label>
+    <Field name="email" type="email"/>
+    Teste!
+  </div>
+  <button type="submit">botão</button>
+  </Form>
+  )}
+/>
+</div>
+)
 
-  const [img, setImg] = useState("");
 
-  function handleChange(e) {
-    setForm({ ...form, [e.target.name]: e.target.value });
+
+
+
+
+
+
+
+  // const [img, setImg] = useState("");
+
+  // function handleChange(e) {
+  //   setForm({ ...form, [e.target.name]: e.target.value });
+  // }
+
+  // function handleImage(e) {
+  //   setImg(e.target.files[0]);
+  // }
+
+  // async function handleUpload() {
+  //   try {
+  //     const uploadData = new FormData();
+  //     uploadData.append("picture", img);
+
+  //     const response = await api.post("/uploadImage", uploadData);
+
+  //     return response.data.url;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // async function handleSubmit(e) {
+  //   e.preventDefault();
+
+  //   try {
+  //     const imgURL = await handleUpload();
+  //     await api.post("/user/signup", { ...form, img: imgURL });
+
+  //     navigate("/login");
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // return (
+
+
+  
+  // )}
+
+
+
+
+
+
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="formName">Nome:</label>
+//       <input
+//         id="formName"
+//         name="name"
+//         type="text"
+//         value={form.name}
+//         onChange={handleChange}
+//       />
+//       <label htmlFor="formImg">Sua foto de perfil:</label>
+//       <input type="file" id="formImg" onChange={handleImage} />
+
+//       <label htmlFor="formEmail">E-mail:</label>
+//       <input
+//         id="formEmail"
+//         name="email"
+//         type="email"
+//         value={form.email}
+//         onChange={handleChange}
+//       />
+//       <label htmlFor="formPassword">Senha:</label>
+//       <input
+//         id="formPassword"
+//         name="password"
+//         type="password"
+//         value={form.password}
+//         onChange={handleChange}
+//       />
+//       <label htmlFor="formConfirmPassword">Confirmação de senha</label>
+//       <input
+//         id="formConfirmPassword"
+//         type="password"
+//         name="confirmPassword"
+//         value={form.confirmPassword}
+//         onChange={handleChange}
+//       />
+//       <button type="submit">Cadastrar</button>
+//     </form>
+//   );
+// }
+
   }
 
-  function handleImage(e) {
-    setImg(e.target.files[0]);
-  }
-
-  async function handleUpload() {
-    try {
-      const uploadData = new FormData();
-      uploadData.append("picture", img);
-
-      const response = await api.post("/upload-image", uploadData);
-
-      return response.data.url;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    try {
-      const imgURL = await handleUpload();
-      await api.post("/user/signup", { ...form, img: imgURL });
-
-      navigate("/login");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="formName">Nome:</label>
-      <input
-        id="formName"
-        name="name"
-        type="text"
-        value={form.name}
-        onChange={handleChange}
-      />
-      <label htmlFor="formImg">Sua foto de perfil:</label>
-      <input type="file" id="formImg" onChange={handleImage} />
-
-      <label htmlFor="formEmail">E-mail:</label>
-      <input
-        id="formEmail"
-        name="email"
-        type="email"
-        value={form.email}
-        onChange={handleChange}
-      />
-      <label htmlFor="formPassword">Senha:</label>
-      <input
-        id="formPassword"
-        name="password"
-        type="password"
-        value={form.password}
-        onChange={handleChange}
-      />
-      <label htmlFor="formConfirmPassword">Confirmação de senha</label>
-      <input
-        id="formConfirmPassword"
-        type="password"
-        name="confirmPassword"
-        value={form.confirmPassword}
-        onChange={handleChange}
-      />
-      <button type="submit">Cadastrar</button>
-    </form>
-  );
-}
