1130f56c2817a7c7eaa18fd5e92597fb6da93745
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-10-05 16:32:47 -0300

    changes

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,88 +1,96 @@
 import { useState } from "react";
 import { api } from "../../api/api";
 import { useNavigate } from "react-router-dom";
-import {Formik, Field, Form} from "formik";
-import style from "./style.module.css"
+import { Formik, Field, Form } from "formik";
+import style from "./style.module.css";
 
 export function Signup() {
+  const navigate = useNavigate();
 
-const navigate=useNavigate();
+  const [files, setFiles] = useState("");
 
-const [files, setFiles] = useState ("");
+  function handleImage(e) {
+    setFiles(e.target.files[0]);
+    console.log(files);
+  }
 
-function handleImage(e) {
-  setFiles(e.target.files[0]);
-  console.log(files)
-}
-
-
-async function handleUpload() {
+  async function handleUpload() {
     try {
-      
       const uploadData = new FormData();
       uploadData.append("pictures", files);
       const response = await api.post("/uploadImage/uploadImage", uploadData);
       return response.data.url;
-    
     } catch (error) {
       console.log(error);
     }
-}
+  }
 
-  async function onSubmit (values, actions){
-    
+  async function onSubmit(values, actions) {
     try {
-            const imgURL = await handleUpload();
-            await api.post("/user/signup", { ...values, birthDate:new Date(values.birthdate), avatar:imgURL });
-            navigate("/login");
-          } catch (error) {
-            console.log(error);
-          }
-}
+      const imgURL = await handleUpload();
+      await api.post("/user/signup", {
+        ...values,
+        birthDate: new Date(values.birthdate),
+        avatar: imgURL,
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-return (<>
-  
-  <div className={style.main}>
-      <div className={style.ihl}>
-        <img src="https://www.frontierfireprotection.com/wp-content/uploads/freshizer/730cbf2e2455c64c961be8e18e793f6b_3-Things-a-Fire-Needs-2000-c-90.jpg" width="200"></img>
-        <h1>Who are you ?</h1>
-        <label>Be a part of community</label>
-      </div>
-  
-  <div className={style.formikeiro}>
-      <Formik 
+  return (
+    <>
+      <div className={style.main}>
+        <div className={style.ihl}>
+          <img
+            src="https://www.frontierfireprotection.com/wp-content/uploads/freshizer/730cbf2e2455c64c961be8e18e793f6b_3-Things-a-Fire-Needs-2000-c-90.jpg"
+            alt="img"
+            width="200"
+          ></img>
+          <h1>Who are you ?</h1>
+          <label>Be a part of community</label>
+        </div>
+
+        <div className={style.formikeiro}>
+          <Formik
             onSubmit={onSubmit}
-              initialValues={{
-              name: "", 
+            initialValues={{
+              name: "",
               email: "",
-              avatar:"",
+              avatar: "",
               birthdate: "",
-              password:""
-      }}         
-      render={({values})=> (
-    <Form>
-      <div className="formFields">
-        <label>Nome</label>
-        <Field name="name" type="text" placeholder="name"/>
-    
-        <label>E-mail</label>
-        <Field name="email" type="email" placeholder="email"/>
+              password: "",
+            }}
+            render={({ values }) => (
+              <Form>
+                <div className="formFields">
+                  <label>Nome</label>
+                  <Field name="name" type="text" placeholder="name" />
 
-        <label>Birthdate</label>
-        <Field name="birthdate" type="date" placeholder="birthdate"/>
+                  <label>E-mail</label>
+                  <Field name="email" type="email" placeholder="email" />
 
-        <label>Password</label>
-        <Field name="password" type="password" placeholder="password"/>
-    </div>
-        <button type="submit">botão</button>
-    </Form>
-  )}
-/>
-  <form>
-    <label htmlFor="formImg">Sua foto de perfil:</label>
-    <input type="file" id="formImg" onChange={handleImage} />
-  </form>
-</div>
-</div>
-</>)
+                  <label>Birthdate</label>
+                  <Field name="birthdate" type="date" placeholder="birthdate" />
+
+                  <label>Password</label>
+                  <Field
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                  />
+                </div>
+                <button type="submit">botão</button>
+              </Form>
+            )}
+          />
+          <form>
+            <label htmlFor="formImg">Sua foto de perfil:</label>
+            <input type="file" id="formImg" onChange={handleImage} />
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
