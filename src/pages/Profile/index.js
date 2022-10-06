44053ffaa40ab84feeff9a499397ef39fd26461c
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2022-10-06 20:11:07 -0300

    user profile need need reviews map

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,29 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/authContext";
+import axios from "axios";
 import style from "./style.module.css"
+import { Card } from "../../components/GameCard";
 
 export function Profile() {
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchCards() {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/1.0/game/games"
+        );
+        
+        setCards([...response.data]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchCards();
+  }, []);
+
 
   const navigate = useNavigate();
 
@@ -25,18 +45,46 @@ export function Profile() {
 
   return (
     <>
-      <div className={style.userProfile}>
-        <h1 className={style.profileTitle}>Taster Profile</h1>
-        <div className={style.userAvatarName}>
-          <img src={loggedInUser.user.avatar} className={style.userAvatar} alt="user avatar"/>
-          <h1 >{loggedInUser.user.name}</h1>
-        </div>
-        <p>{loggedInUser.user.birthDate}</p>
-        <p>{loggedInUser.user.email}</p>
-        <button onClick={handleCreateGame}>Display a new Game</button>
-        <div className={style.Buttons}>
-          <button onClick={handleLogOut}>Logout</button>
-          <button onClick={handleEditProfile}>Edit</button>
+      <div className={style.userProfilePage}>
+        <div className={style.userProfile}>
+          <h1 className={style.profileTitle}>Taster Profile</h1>
+          <div className={style.userPersonalInfo}>
+            <div className={style.userAvatarName}>
+              <img src={loggedInUser.user.avatar} className={style.userAvatar} alt="user avatar"/>
+              <h1>{loggedInUser.user.name}</h1>
+            </div>
+            <p>Birth date: {loggedInUser.user.birthDate}</p>
+            <p>Email: {loggedInUser.user.email}</p>
+            <button className={style.buttons} onClick={handleEditProfile}>Edit</button>
+          </div>
+          <div className={style.gamesServed}>
+            <h2 className={style.subTitles}>Games you served</h2>
+            <button className={style.buttons} onClick={handleCreateGame}>Serve a new Game</button>
+            <div className={style.cardsContainer}>
+              {cards
+                .slice(0)
+                .reverse()
+                .map((currentCard) => {
+                  if (loggedInUser.user._id === currentCard.owner._id) {
+                  return (
+                    <>
+                      <Link to={`/game/${currentCard._id}`}>
+                        <Card
+                          title={currentCard.title}
+                          gameLogo={currentCard.gameLogo}
+                          owner={currentCard.owner.name}
+                        />
+                      </Link>
+                    </>
+                  );
+                  }
+                })}
+            </div>
+          </div>
+          <div className={style.reviewsMade}>
+            <h2 className={style.subTitles}>Reviews you made</h2>
+          </div>
+          <button className={style.buttons} onClick={handleLogOut}>Logout</button>
         </div>
       </div>
     </>
