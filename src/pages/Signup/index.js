c431c147f82e7812269aa657b449554fada075a5
maxwell-paulo <maxpaulo.mps@gmail.com>
2022-10-07 10:06:43 -0300

    singup page style

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -14,7 +14,7 @@ export function Signup() {
     console.log(files);
   }
 
-async function handleUpload() {
+  async function handleUpload() {
     try {
       const uploadData = new FormData();
       uploadData.append("pictures", files);
@@ -41,18 +41,15 @@ async function handleUpload() {
 
   return (
     <>
-      <div className={style.main}>
-        <div className={style.ihl}>
-          <img
+      <div className={style.container}>
+        {/* <img
             src="https://www.frontierfireprotection.com/wp-content/uploads/freshizer/730cbf2e2455c64c961be8e18e793f6b_3-Things-a-Fire-Needs-2000-c-90.jpg"
             alt="img"
             width="200"
-          ></img>
-          <h1>Who are you ?</h1>
-          <label>Be a part of community</label>
-        </div>
+          ></img> */}
+        <h1 className={style.h1}>Create new Chef or Taster</h1>
 
-        <div className={style.formikeiro}>
+        <div className={style.formik}>
           <Formik
             onSubmit={onSubmit}
             initialValues={{
@@ -64,31 +61,53 @@ async function handleUpload() {
             }}
             render={({ values }) => (
               <Form>
-                <div className="formFields">
-                  <label>Nome</label>
-                  <Field name="name" type="text" placeholder="name" />
+                <div className={style.form}>
+                  <div className={style.formBlock}>
+                    <div className={style.formField}>
+                      <label>Name: </label>
+                      <Field name="name" type="text" placeholder="name" />
+                    </div>
 
-                  <label>E-mail</label>
-                  <Field name="email" type="email" placeholder="email" />
+                    <div className={style.formField}>
+                      <label>E-mail: </label>
+                      <Field name="email" type="email" placeholder="email" />
+                    </div>
+                  </div>
 
-                  <label>Birthdate</label>
-                  <Field name="birthdate" type="date" placeholder="birthdate" />
+                  <div className={style.formBlock}>
+                    <div className={style.formField}>
+                      <label>Birthdate: </label>
+                      <Field
+                        name="birthdate"
+                        type="date"
+                        placeholder="birthdate"
+                      />
+                    </div>
 
-                  <label>Password</label>
-                  <Field
-                    name="password"
-                    type="password"
-                    placeholder="password"
-                  />
+                    <div className={style.formField}>
+                      <label>Password: </label>
+                      <Field
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                      />
+                    </div>
+                  </div>
+
+                  <div className={style.formField}>
+                    <form>
+                      <label htmlFor="formImg">Profile picture: </label>
+                      <input type="file" id="formImg" onChange={handleImage} />
+                    </form>
+                  </div>
+
+                  <button type="submit" className={style.button}>
+                    <spam className={style.anima}>CREATE USER</spam>
+                  </button>
                 </div>
-                <button type="submit">botão</button>
               </Form>
             )}
           />
-          <form>
-            <label htmlFor="formImg">Sua foto de perfil:</label>
-            <input type="file" id="formImg" onChange={handleImage} />
-          </form>
         </div>
       </div>
     </>
