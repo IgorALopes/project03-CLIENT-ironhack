7a15d93d3f3c453a28c32936a7c37e30fbb86fab
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2022-10-07 07:41:12 -0300

    no Carousel in game page

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -6,7 +6,6 @@ import { useParams, useNavigate } from "react-router-dom";
 import { api } from "../../api/api";
 import { ReviewPopUp } from "../../components/ReviewPopUp";
 import { data } from "autoprefixer";
-import Carousel from 'react-bootstrap/Carousel';
 
 export function Game() {
   const [triggingReview, setTrigging] = useState(false);
@@ -88,50 +87,6 @@ export function Game() {
     <>
       <div>
         <h1 className={style.sSV}>Screenshots</h1>
-        <div>
-          <Carousel>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Second slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl
-                  consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        </div>
         <div className={style.shots}>
           {screenShotView ? (
             screenShotView.map((current) => {
